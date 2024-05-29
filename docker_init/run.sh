15a062d69c42a1a7e589bd2adc0612269ee2465d
LOLOLEKIK <loris.thiebaut@gmail.com>
2024-05-29 14:07:55 +0200

    fix env variables

--- docker_init/run.sh
+++ docker_init/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 sed -i "s/TOREPLACE/${SERVER_NAME}/g" /etc/apache2/sites-available/000-default.conf
+echo "SetEnv MYSQL_DATABASE ${MYSQL_DATABASE}" >> /etc/apache2/apache2.conf
+echo "SetEnv MYSQL_USER ${MYSQL_USER}" >> /etc/apache2/apache2.conf
+echo "SetEnv MYSQL_PASSWORD ${MYSQL_PASSWORD}" >> /etc/apache2/apache2.conf
 apt update
 apt install php-mbstring -y
 a2enmod rewrite
